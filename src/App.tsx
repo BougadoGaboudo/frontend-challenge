ba3e52392600dff6f52688a3df6fc104dd4e3f01
Bougado <bougadowilliam@gmail.com>
2025-08-03 18:29:41 +0200

    feat(app): integrate components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Container } from "@mui/material";
+import Header from "./components/Header";
+import SystemAlert from "./components/SystemAlert";
+import { useRef } from "react";
+import CampaignForm, {
+  type CampaignFormValues,
+} from "./components/CampaignForm";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const formRef = useRef<{ submit: () => void }>(null);
+
+  const handleSubmit = (data: CampaignFormValues) => {
+    console.log("Data du form :", data);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header onSaveClick={() => formRef.current?.submit()} />
+      <Container maxWidth="xl">
+        <SystemAlert />
+        <CampaignForm ref={formRef} profile="PREMIUM" onSubmit={handleSubmit} />
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
